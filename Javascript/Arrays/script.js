50ade31b811f8009dac263f39daaf8d79b90aae0
Victor Carreras <ctt.victorcarreras@gmail.com>
2023-04-25 15:35:57 -0300

    Arrays com JavaScript

---
+++ Javascript/Arrays/script.js
@@ -0,0 +1,17 @@
+const alunos = [
+    {
+        nome: 'Victor',
+        idade: 21,
+    },
+    {
+        nome: 'Erenilda',
+        idade: 23,
+    },
+];
+
+alunos.push({
+        nome: 'Joao',
+        idade: 33,
+},)
+
+console.log(alunos[2].idade);
